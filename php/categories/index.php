73a5220a807b105b182f7a727c8c1e1481d3b2a3
Ravi <rld.dobariya@gmail.com>
2013-12-06 12:24:10 -0500

    d

--- php/categories/index.php
+++ php/categories/index.php
@@ -6,13 +6,10 @@ $aUrls = explode('/', $_SERVER['REQUEST_URI']);
 
 $nId = "";
 $sTable = "";
-print_r($aUrls);
 $nExtra =  array_pop($aUrls);
-//$nId = $nExtra;
 
 if($nExtra == "")
 {
-	echo "i am in A";
 	$first = array_pop($aUrls);
 	if(is_numeric($first))
 	{
@@ -25,13 +22,11 @@ if($nExtra == "")
 	}
 }else if(is_numeric($nExtra))
 {
-	echo "i am in B";
 	$nId = $nExtra;
 	$sTable = array_pop($aUrls);
 }
 else
 {
-	echo "i am in C";
 	$sTable = $nExtra;
 }
 
