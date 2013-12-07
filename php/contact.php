84fdde35ee5e1e4dec4037db7bf636ef30b2b76e
Ravi <rld.dobariya@gmail.com>
2013-12-07 00:08:15 -0500

    k

--- php/contact.php
+++ php/contact.php
@@ -1,22 +1,22 @@
 <?php
 
 $db = new PDO ( "mysql:host=127.8.66.130;dbname=vegetablemart;port=3306","adminunkFGYh", "6gAqvZF7i1ak" );
-if($db)
-	echo "connected";
-echo count($_POST);
 print_r($_POST);
 $fname = $_POST['fname'];
-echo $fname;
 $lname = $_POST['lname'];
-echo $lname;
 $email = $_POST['email'];
-echo $email;
 $detail = $_POST['comments'];
-echo $detail;
-
 $stmt = $db->prepare("INSERT INTO inquiry (firstName,lastName,email,comments) VALUES(?,?,?,?)");
 $stmt->execute(array($fname,$lname,$email,$detail));
 $count = $stmt->rowCount();
-echo "inserted rows".$count;
+if($count == 1)
+{
+	echo "Thanks for your information. We will contact you soon.";
+}
+else 
+{
+	echo "We are sorry for inconviniance, we could not process your request this time, please try again letter.";
+}
+
 
 ?>
