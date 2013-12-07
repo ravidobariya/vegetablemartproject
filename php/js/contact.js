b7d7fe98046e0b40dac35d3634fe5d5f70997c28
Ravi <rld.dobariya@gmail.com>
2013-12-07 00:15:28 -0500

    d

---
+++ php/js/contact.js
@@ -0,0 +1,28 @@
+jQuery("document").ready(function(){
+	var validate = true;
+	if(isNaN($(#fname).value()) || isNaN($(#lname).value()) || isNaN($(#email).value()) || isNaN($(#comments).value()))
+	{
+		validate = false;
+	}
+	$("#contact").submit(function(){
+		if(validate)
+		{
+			$.ajax({url:"contact.php",data : {
+			fname:$("#fname").val(),
+			lname:$("#lname").val(),
+			email:$("#email").val(),
+			comments:$("#comments").val()
+			},
+			type : "post"
+			})
+			.done(function(data){
+			$("#message").html(data);
+			});
+			return(false);
+		}
+		else
+		{
+			$("#message").html("Please Enter Valid input");
+		}
+	});	
+});
