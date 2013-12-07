1e6f350b4189cbecafaac04937f5da71904c8ece
Ravi <rld.dobariya@gmail.com>
2013-12-06 22:39:58 -0500

    k

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php
+
+$db = new PDO ( "mysql:host=127.8.66.130;dbname=vegetablemart;port=3306","adminunkFGYh", "6gAqvZF7i1ak" );
+echo $fname = $_POST('fname');
+echo $lname = $_POST('lname');
+echo $email = $_POST('email');
+echo $detail = $_POST('comments');
+
+$stmt = $db->prepare("INSERT INTO Inquiry (firstName,lastName,email,comments) VALUES ($fname,$lname,$email,$detail)");
+$stmt->execute();
+$count = $stmt->rowCount();
+echo "inserted rows".$count;
+
+?>
