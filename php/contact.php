7aee640341c3c046b17e602da67a1355fa3af905
Ravi <rld.dobariya@gmail.com>
2013-12-13 13:08:26 -0500

    df

--- php/contact.php
+++ php/contact.php
@@ -1,7 +1,6 @@
 <?php
 
 $db = new PDO ( "mysql:host=127.8.66.130;dbname=vegetablemart;port=3306","adminunkFGYh", "6gAqvZF7i1ak" );
-print_r($_POST);
 $fname = $_POST['fname'];
 $lname = $_POST['lname'];
 $email = $_POST['email'];
