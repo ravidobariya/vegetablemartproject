9042c88beb4c3f465631c554357239fe21606a57
Ravi <rld.dobariya@gmail.com>
2013-12-12 20:39:20 -0500

    sdf

--- php/contact.php
+++ php/contact.php
@@ -9,15 +9,6 @@ $detail = $_POST['comments'];
 $stmt = $db->prepare("INSERT INTO inquiry (firstName,lastName,email,comments) VALUES(?,?,?,?)");
 $stmt->execute(array($fname,$lname,$email,$detail));
 $count = $stmt->rowCount();
-header("Location:http://")
-if($count == 1)
-{
-	echo "Thanks for your information. We will contact you soon.";
-}
-else 
-{
-	echo "We are sorry for inconviniance, we could not process your request this time, please try again letter.";
-}
-
+header("Location:http://vegetablemart-cadfinal.rhcloud.com/#submit");
 
 ?>
